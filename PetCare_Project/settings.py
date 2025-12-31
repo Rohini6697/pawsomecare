2662fcd0cc1e4527803c0f863ba7409c5b823323
Rohini Sureshbabu <rohinimrd@gmail.com>
2025-12-31 10:25:25 +0530

    created gitignore

--- PetCare_Project/settings.py
+++ PetCare_Project/settings.py
@@ -126,5 +126,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'pawsomecare.online@gmail.com'
-EMAIL_HOST_PASSWORD = 'fezs nvbi lzgv hyhf'
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 DEFAULT_FROM_EMAIL = 'Pawsome Care <pawsomecare.online@gmail.com>'
